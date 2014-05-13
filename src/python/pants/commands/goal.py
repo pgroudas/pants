1b46344fba5a59853f303694be295a48e04d8459
Patrick Lawson <pl@foursquare.com>
2014-05-13 15:15:16 -0400

    Remove loud workunit

--- src/python/pants/commands/goal.py
+++ src/python/pants/commands/goal.py
@@ -154,9 +154,8 @@ class Goal(Command):
       with self.run_tracker.new_workunit(name='parse', labels=[WorkUnit.SETUP]):
         for spec in specs:
           for address in spec_parser.parse_addresses(spec):
-            with self.run_tracker.new_workunit(name='inject-spec', labels=[WorkUnit.SETUP]):
-              self.build_file_parser.inject_spec_closure_into_build_graph(address.spec,
-                                                                          self.build_graph)
+            self.build_file_parser.inject_spec_closure_into_build_graph(address.spec,
+                                                                        self.build_graph)
             self.targets.append(self.build_graph.get_target(address))
     self.phases = [Phase(goal) for goal in goals]
 
