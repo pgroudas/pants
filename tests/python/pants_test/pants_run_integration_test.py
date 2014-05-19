4018309daa70826d48c1b74de1ab1feec16ee847
Tejal Desai <tdesai@twitter.com>
2014-05-19 09:43:23 -0700

    fix code review

--- tests/python/pants_test/pants_run_integration_test.py
+++ tests/python/pants_test/pants_run_integration_test.py
@@ -7,6 +7,7 @@ from __future__ import (nested_scopes, generators, division, absolute_import, wi
 
 import optparse
 import os
+import subprocess
 import unittest
 
 from contextlib import contextmanager
@@ -14,13 +15,6 @@ from tempfile import mkdtemp
 from textwrap import dedent
 
 from twitter.common.dirutil import Lock, safe_open, safe_rmtree
-
-from pants.base.config import Config
-from pants.base.build_environment import get_buildroot
-from pants.commands.goal import Goal
-from pants.goal.initialize_reporting import initial_reporting
-from pants.goal.run_tracker import RunTracker
-from pants.tasks.nailgun_task import NailgunTask
 
 from mock import patch
 
@@ -32,9 +26,6 @@ class PantsRunIntegrationTest(unittest.TestCase):
 
   @classmethod
   def setUp(self):
-    self.parser = optparse.OptionParser(add_help_option=False)
-    self.config = Config.load()
-    self.run_tracker = RunTracker.from_config(self.config)
     self.pants_workdir = mkdtemp(suffix='PANTS_WORK_DIR')
     ini = dedent('''
           [DEFAULT]
@@ -42,29 +33,17 @@ class PantsRunIntegrationTest(unittest.TestCase):
           ''' % dict(workdir=self.pants_workdir))
 
     with safe_open(os.path.join(self.pants_workdir, 'pants.ini'), mode='w') as fp:
-      fp.write(ini)
-    self.runfile = os.path.join(self.pants_workdir, '.pants.run')
+       fp.write(ini)
 
   @contextmanager
   def run_pants(self, command_args=None):
     with patch.dict('os.environ', {'PANTS_CONFIG_OVERRIDE': os.path.join(self.pants_workdir,
-                                                                         'pants.ini')}):
-      report = initial_reporting(self.config, self.run_tracker)
-      self.run_tracker.start(report)
-      self.command = Goal(self.run_tracker,
-                     get_buildroot(),
-                     self.parser,
-                     command_args)
-      try:
-        self.lock = Lock.acquire(self.runfile)
-        result = self.command.run(self.lock)
-        yield result
-      finally:
-        self.lock.release()
-        self.run_tracker.end()
+                                                                         'pants.ini'),
+                                   'PANTS_DEV': '1'}):
+      pants_commands = ['./pants', 'goal'] + command_args
+      result = subprocess.call(pants_commands)
+      yield result
 
   @classmethod
   def tearDown(self):
-    NailgunTask._DAEMON_OPTION_PRESENT = False
-    print("Removing %s " %self.pants_workdir)
     safe_rmtree(self.pants_workdir)
--- tests/python/pants_test/tasks/test_jar_publish_integration.py
+++ tests/python/pants_test/tasks/test_jar_publish_integration.py
@@ -6,6 +6,7 @@ from __future__ import (nested_scopes, generators, division, absolute_import, wi
 
 import os
 
+from twitter.common.contextutil import temporary_dir
 from twitter.common.dirutil import safe_mkdtemp, safe_rmtree
 from pants_test.pants_run_integration_test import PantsRunIntegrationTest
 
@@ -16,37 +17,36 @@ from mock import MagicMock, patch
 class JarPublishIntegrationTest(PantsRunIntegrationTest):
 
   def test_scala_publish(self, MagicMock):
-    self.package_namespace = 'com/pants/example/jvm-example-lib/0.0.1-SNAPSHOT'
     self.publish_test('src/scala/com/pants/example/BUILD:jvm-run-example-lib',
+                      'com/pants/example/jvm-example-lib/0.0.1-SNAPSHOT',
                       ['ivy-0.0.1-SNAPSHOT.xml',
                        'jvm-example-lib-0.0.1-SNAPSHOT.jar',
                        'jvm-example-lib-0.0.1-SNAPSHOT.pom',
                        'jvm-example-lib-0.0.1-SNAPSHOT-javadoc.jar',
-                       'jvm-example-lib-0.0.1-SNAPSHOT-sources.jar'])
+                       'jvm-example-lib-0.0.1-SNAPSHOT-sources.jar'],)
 
 
   def test_scala_publish1(self, MagicMock):
-    self.package_namespace = 'com/pants/example/hello-greet/0.0.1-SNAPSHOT/'
     self.publish_test('src/java/com/pants/examples/hello/greet',
+                      'com/pants/examples/hello-greet/0.0.1-SNAPSHOT/',
                       ['ivy-0.0.1-SNAPSHOT.xml',
                        'hello-greet-0.0.1-SNAPSHOT.jar',
                        'hello-greet-0.0.1-SNAPSHOT.pom',
                        'hello-greet-0.0.1-SNAPSHOT-javadoc.jar',
                        'hello-greet-0.0.1-SNAPSHOT-sources.jar'])
 
-  def publish_test(self, target, artifacts=[]):
-    dir = safe_mkdtemp()
-    with patch('__builtin__.raw_input', return_value='Y'):
-      with self.run_pants(['publish', target,
-                           '--publish-local=%s' % dir,
+  def publish_test(self, target, package_namepsace, artifacts=[]):
+    with temporary_dir() as publish_dir:
+      with patch('__builtin__.raw_input', return_value='Y'):
+        with self.run_pants(['publish', target,
+                           '--publish-local=%s' % publish_dir,
                            '--no-publish-dryrun',
                            '--no-publish-commit',
                            '--publish-force',
                            '--publish-jar_create_publish-sources']) as pants_run:
-        for file in artifacts:
-          self.assertTrue(os.path.exists(os.path.join(dir,
-                                                      self.package_namespace,
-                                                      file)))
+          for file in artifacts:
+            self.assertTrue(os.path.exists(os.path.join(publish_dir,
+                                                        package_namepsace,
+                                                        file)))
     self.assertEquals(pants_run, self.PANTS_SUCCESS_CODE)
-    safe_rmtree(dir)
 
