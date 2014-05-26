471c4b3e5217bff80d705d314e3548dba4baabad
Patrick Lawson <pl@foursquare.com>
2014-05-26 17:07:49 -0400

    Resources comment

--- src/python/pants/targets/resources.py
+++ src/python/pants/targets/resources.py
@@ -9,6 +9,8 @@ from pants.base.payload import ResourcesPayload
 from pants.base.target import Target
 
 
+# TODO(pl): This isn't JVM only anymore, Python targets are also being transitioned
+# over to use this Resource Target
 @manual.builddict(tags=['jvm'])
 class Resources(Target):
   """A set of files accessible as resources from the JVM classpath.
