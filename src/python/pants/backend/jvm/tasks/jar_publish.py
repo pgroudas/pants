ad5d5ed6443b81f15b11c676cfb2bbeb9c6f6d71
Patrick Lawson <pl@foursquare.com>
2014-07-15 17:21:55 -0700

    Doh

--- src/python/pants/backend/jvm/tasks/jar_publish.py
+++ src/python/pants/backend/jvm/tasks/jar_publish.py
@@ -544,8 +544,8 @@ class JarPublish(JarTask, ScmPublish):
       if no_changes:
         changelog = 'No changes for %s - forced push.\n' % jar_coordinate(jar, semver.version())
       else:
-        print(changelog)
         changelog = self.changelog(target, sha) or 'Direct dependencies changed.\n'
+      print(changelog)
 
       if no_changes and not self.force:
         print('No changes for %s' % jar_coordinate(jar, semver.version()))
