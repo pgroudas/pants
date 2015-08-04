80b066cfc6d238f3cf103d99a210ac2213e1362a
Paul Groudas <me@paulgroudas.com>
2015-08-03 23:33:51 -0400

    Prepare for release 0.0.39-compass-20150803-01
    This includes a backport from 0.0.41 to fix python chroot caching.

--- src/python/pants/version.py
+++ src/python/pants/version.py
@@ -6,4 +6,4 @@ from __future__ import (absolute_import, division, generators, nested_scopes, pr
                         unicode_literals, with_statement)
 
 
-VERSION = '0.0.39-compass-20150731-02'
+VERSION = '0.0.39-compass-20150803-01'
